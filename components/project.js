e540d89727c7b3bcee0960a34667551200d086b4
rubenberna <rubenmbernardes@gmail.com>
2021-05-28 19:11:47 +0200

    Better messages

--- components/project.js
+++ components/project.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { render, Box, Text, useInput, useApp, Static } from 'ink';
 import TextInput from 'ink-text-input';
+import Spinner from 'ink-spinner';
 import { clone } from '../utils/gitClone';
 import { authenticate } from '../utils/auth';
 
 const SearchQuery = () => {
+  const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState('');
-  const [userIsAuthenticated, setUserIsAuthenticated] = useState(false)
-  const [steps, setSteps] = useState([])
+  const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
+  const [steps, setSteps] = useState([]);
+  const [completed, setCompleted] = useState(false);
   const {exit} = useApp();
 
   const updateSteps = (newStep) => {
@@ -18,6 +21,7 @@ const SearchQuery = () => {
   }
 
   const auth = async () => {
+    setLoading(true)
     await authenticate()
     setUserIsAuthenticated(true);
     updateSteps({
@@ -25,23 +29,27 @@ const SearchQuery = () => {
       title: 'Authenticated',
       success: true
     })
-  }
+    setLoading(false)
+  };
 
-  useEffect(() => {
+  useEffect( () => {
     auth()
-  }, [])
+  }, []);
   
   useInput((input, key) => {
     if (key.return) {
+      setLoading(true)
       clone(query)
       updateSteps({
         id: 'clone',
         title: 'Cloned repo',
         success: true
       })
+      setLoading(false)
+      setCompleted(true)
       exit()
     }
-  })
+  });
 
   const renderDialog = () => {
     return (
@@ -55,7 +63,22 @@ const SearchQuery = () => {
         </Box>
       </>
     );
-  }
+  };
+
+  const renderCompleted = () => (
+    <Box borderStyle="round" borderColor="green" width={40} alignItems="center" padding={1}>
+      <Text>If you build it, they will come!!</Text>
+    </Box>
+  );
+
+  const renderSpinner = () => (
+    <Text>
+      <Text color="green">
+        <Spinner type="dots" />
+      </Text>
+      {' Loading'}
+    </Text>
+  );
 
   return (
     <>
@@ -68,7 +91,15 @@ const SearchQuery = () => {
       </Static>
 
       {
+        loading && renderSpinner()
+      }
+
+      {
         userIsAuthenticated && renderDialog()
+      }
+
+      {
+        completed && renderCompleted()
       }
     </>
   )
