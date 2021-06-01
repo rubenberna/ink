3beca4a4ea19e675327d00bb18419886af862fc7
Ruben Bernardes <36078374+rubenberna@users.noreply.github.com>
2021-06-01 20:37:17 +0200

    Feature/vstms (#15)
    * Removed unused components
    
    * Added step
    
    * Better messages
    
    * Connect to feed via vsts-npm-auth if Windows
    
    Co-authored-by: Ruben <ruben.bernardes@consultant.volvo.com>

--- hooks/useRunProject.js
+++ hooks/useRunProject.js
@@ -1,25 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { STEPS, getStepsDetails } from '../consts/steps.consts';
 import { DataToolGenerator } from '../utils/dataToolTemplate.util';
 import { authenticate } from '../utils/auth.util';
-import { STEPS } from '../consts/steps.consts';
+import { installVstsAuth } from '../utils/npmrc'
 
 export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthenticated, setCompleted, projectName, manager, exit) => {
-  const [os, setOs] = useState(undefined);
-
   const startAuth = async () => {
     await _auth()
     await _getOs()
   }
 
   const getDataTool = async () => {
-    const dataToolGenerator = new DataToolGenerator(manager, projectName, os)
+    const dataToolGenerator = new DataToolGenerator(manager, projectName)
     await _cloneProject(dataToolGenerator)
     await _installProject(dataToolGenerator)
     _finish()
   }
 
   useEffect(() => {
-    if (steps.length === STEPS.CLONE.nr) {
+    if (steps.length === getStepsDetails(STEPS.CLONE).nr) {
       getDataTool()
     }
   }, [steps])
@@ -28,18 +27,26 @@ export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthen
     setLoadingMsg('Authenticating')
     await authenticate()
     setUserIsAuthenticated(true);
-    updateSteps(STEPS.AUTH)
+    updateSteps(getStepsDetails(STEPS.AUTH))
     setLoadingMsg(undefined)
   }
 
   const _getOs = async () => {
+    setLoadingMsg('Setting connection to feed')
     const os = process.platform
-    setOs(os)
+    if (os === 'win32') {
+      await installVstsAuth()
+      updateSteps(getStepsDetails(STEPS.NPMRC))
+      setLoadingMsg(undefined)
+    } else {
+      updateSteps(getStepsDetails(STEPS.NPMRC, false, 'As you are not a Windows user, please check how to setup authentication to the feed here: https://docs.microsoft.com/en-us/azure/devops/artifacts/npm/npmrc?view=azure-devops#set-up-authentication-on-your-dev-box'))
+      setLoadingMsg(undefined)
+    }
   }
 
   const _cloneProject = async (dataToolGenerator) => {
     await dataToolGenerator.cloneProject()
-    return updateSteps(STEPS.CLONE)
+    return updateSteps(getStepsDetails(STEPS.CLONE))
   }
 
   const _installProject = async (dataToolGenerator) => {
@@ -49,7 +56,7 @@ export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthen
   }
 
   const _finish = () => {
-    updateSteps(STEPS.INSTALLATION)
+    updateSteps(getStepsDetails(STEPS.INSTALLATION))
     setLoadingMsg(undefined)
     setCompleted(true)
     exit()
--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-require('dotenv').config()
-require('@babel/register')
+require('dotenv').config();
+require('@babel/register');
 require("babel-polyfill");
-// require('./components/selection')
-// require('./components/pkg')
-// require('./components/static')
-// require('./components/exit')
-// require('./components/arkanoid')
-require('./components/App')
+require('./components/App');
