21c954e954a45d6f1c004a4fa03d5d15270f63b3
Ruben Bernardes <36078374+rubenberna@users.noreply.github.com>
2021-06-01 10:25:48 +0200

    Dev (#9)
    * Auth (#2)
    
    * Feature/change input (#3)
    
    * Auth
    
    * Silent clone
    
    * Feature/change input (#4)
    
    * Auth
    
    * Silent clone
    
    * Better messages
    
    * Added loading and success msg
    
    * Helpers (#6)
    
    * Feature/helpers (#7)
    
    * Helpers
    
    * Select package manager
    
    * Feature/helpers (#8)
    
    * Helpers
    
    * Select package manager
    
    * Fixed manager

--- components/App.js
+++ components/App.js
@@ -1,55 +1,83 @@
 import React, { useState, useEffect } from 'react';
 import { render, Box, Text, useInput, useApp, Static, Newline } from 'ink';
 import TextInput from 'ink-text-input';
-import { clone } from '../utils/gitClone';
-import { authenticate } from '../utils/auth';
 import Spinner from 'ink-spinner';
+import SelectInput from 'ink-select-input';
+import DataToolTemplateUtil from '../utils/dataToolTemplate.util';
+import { STEPS } from '../consts/steps.consts';
 
 const RunProject = () => {
+  const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
   const [projectName, setProjectName] = useState('');
-  const [loadingMsg, setLoadingMsg] = useState(undefined)
-  const [userIsAuthenticated, setUserIsAuthenticated] = useState(false)
-  const [steps, setSteps] = useState([])
-  const [completed, setCompleted] = useState(false)
+  const [manager, setManager] = useState(undefined);
+  const [loadingMsg, setLoadingMsg] = useState(undefined);
+  const [steps, setSteps] = useState([]);
+  const [completed, setCompleted] = useState(false);
   const {exit} = useApp();
+
+  console.log({manager});
+
+  useEffect(() => {
+    _auth()
+  }, []);
+
+  useInput((input, key) => {
+    if (key.return && steps.length === STEPS.PROJECT_NAME.nr) {
+      return _addProjectName()
+    }
+    if (key.return && steps.length === STEPS.MANAGER.nr) {
+      return _addPackageManagerAndCloneProject(manager)
+    }
+  });
 
   const updateSteps = (newStep) => {
     setSteps(previousSteps => [
       ...previousSteps,
       newStep
     ])
+  };
+
+  const updatePackageManager = (item) => {
+    setManager(item.value)
+    return _addPackageManagerAndCloneProject(item.value)
   }
 
-  const auth = async () => {
+  const _auth = async () => {
     setLoadingMsg('Authenticating')
-    await authenticate()
+    // await DataToolTemplateUtil.auth()
     setUserIsAuthenticated(true);
-    updateSteps({
-      id: 'auth',
-      title: 'Authenticated',
-      success: true
-    })
+    updateSteps(STEPS.AUTH)
     setLoadingMsg(undefined)
   }
 
-  useEffect(() => {
-    auth()
-  }, [])
-  
-  useInput((input, key) => {
-    if (key.return) {
-      setLoadingMsg('Cloning repo')
-      clone(projectName)
-      updateSteps({
-        id: 'clone',
-        title: 'Cloned repo',
-        success: true
-      })
-      setLoadingMsg(undefined)
-      setCompleted(true)
-      exit()
+  const _addProjectName = () => {
+    updateSteps(STEPS.PROJECT_NAME)
+  }
+
+  const _addPackageManagerAndCloneProject = async (manager) => {
+    setLoadingMsg('Preparing data tool')
+    updateSteps(STEPS.MANAGER)
+    await DataToolTemplateUtil.cloneProject(projectName)
+    updateSteps(STEPS.CLONE)
+    setLoadingMsg(undefined)
+    return _installProject(projectName, manager)
+  }
+
+  const _installProject = async (dest, manager) => {
+    setLoadingMsg('Installing dependencies')
+    if (manager) {
+      await DataToolTemplateUtil.installApp(dest, manager)
+      await DataToolTemplateUtil.installWorkbench(manager)
+      _finish()
     }
-  })
+  }
+
+  const _finish = () => {
+    updateSteps(STEPS.INSTALLATION)
+    setLoadingMsg(undefined)
+    setCompleted(true)
+    exit()
+  }
 
   const renderLoading = () => {
     return loadingMsg &&
@@ -61,41 +89,68 @@ const RunProject = () => {
       </Text>
   }
 
-  const renderDialog = () => {
-    return steps.length <= 1 && (
-      <Box>
-        <Box marginRight={1}>
-          <Text>Enter the name of your project:</Text>
-        </Box>
+  const renderSelectManager = () => {
+    const items = [
+      {
+        label: 'Yarn',
+        value: 'yarn'
+      },
+      {
+        label: 'Npm',
+        value: 'npm'
+      }
+    ]
 
-        <TextInput value={projectName} onChange={setProjectName} />
+    return (
+      <Box flexDirection="column">
+        <Text>What's your favourite package manager?</Text>
+        <SelectInput items={items} onSelect={updatePackageManager}/>
       </Box>
     )
   }
 
-  const renderSuccess = () => (
-    <Box borderStyle="round" borderColor="green" width={40} padding={2}>
-      <Text>
-        <Text>cd <Text color="green" bold>{projectName}</Text></Text>
-        <Newline />
-        <Newline />
-        <Text>If you build it, they will come!</Text>
-      </Text>
+  const renderDialog = () => (
+    <Box>
+      <Box marginRight={1}>
+        <Text>Enter the name of your project:</Text>
+      </Box>
+
+      <TextInput value={projectName} onChange={setProjectName} />
     </Box>
   )
 
+  const renderSuccess = () => (
+    <Box borderStyle="round" borderColor="green" width={40} padding={2}>
+      <Text>
+        <Text italic>To start your project:</Text>
+        <Newline/>
+        <Text>cd <Text color="#006064" bold>{projectName}</Text></Text>
+        <Newline />
+        <Text color="#006064" bold>{manager} start</Text>
+        <Newline />
+        <Newline />
+        <Text italic>To publish your project:</Text>
+        <Newline/>
+        <Text color="#006064" bold>{manager} publish</Text>
+        <Newline />
+        <Newline />
+        <Text italic>"If you build it, they will come!"</Text>
+      </Text>
+    </Box>
+  )
 
   return (
     <>
       <Static items={steps}>
         {step => (
-          <Box key={step.id}>
+          <Box key={step.nr}>
             <Text color="green">✔ {step.title}</Text>
           </Box>
         )}
       </Static>
       { loadingMsg && renderLoading() }
-      { userIsAuthenticated && renderDialog() }
+      { (userIsAuthenticated && steps.length === STEPS.PROJECT_NAME.nr )&& renderDialog() }
+      { (userIsAuthenticated && steps.length === STEPS.MANAGER.nr )&& renderSelectManager() }
       { completed && renderSuccess() }
     </>
   )
---
+++ utils/dataToolTemplate.util.js
@@ -0,0 +1,29 @@
+import execa from 'execa';
+import { authenticate } from './auth.util';
+import { clone } from './clone.util';
+
+const DataToolTemplateUtil = (() => {
+
+  const _cloneProject = (projectName) => {
+    clone(projectName)
+  }
+
+  const _installApp = (dest, manager) => {
+    process.chdir(dest)
+    return execa(manager, ['install'])
+  }
+
+  const _installWorkbench = (manager) => {
+    process.chdir('workbench')
+    return execa(manager, ['install'])
+  }
+
+  return {
+    auth: authenticate,
+    cloneProject: _cloneProject,
+    installApp: _installApp,
+    installWorkbench: _installWorkbench
+  }
+})()
+
+export default DataToolTemplateUtil;
