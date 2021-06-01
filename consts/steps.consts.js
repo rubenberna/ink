2effeb3a46acebc2da25ae7dca67aea22d324891
rubenberna <rubenmbernardes@gmail.com>
2021-06-01 20:25:20 +0200

    Better messages

--- consts/steps.consts.js
+++ consts/steps.consts.js
@@ -1,32 +1,74 @@
+// export const STEPS = {
+//   AUTH: {
+//     nr: 0,
+//     title: 'Authenticated',
+//     success: true
+//   },
+//   NPMRC: {
+//     nr: 1,
+//     title: 'Install vsts-npm-auth',
+//     success: true
+//   },
+//   PROJECT_NAME: {
+//     nr: 2,
+//     title: 'Project name',
+//     success: true
+//   },
+//   MANAGER:  {
+//     nr: 3,
+//     title: 'Selected package manager',
+//     success: true
+//   },
+//   CLONE:  {
+//     nr: 4,
+//     title: 'Cloned repo',
+//     success: true
+//   },
+//   INSTALLATION:  {
+//     nr: 5,
+//     title: 'Installed dependencies',
+//     success: true
+//   },
+// }
+
 export const STEPS = {
-  AUTH: {
+  AUTH: 'AUTH',
+  NPMRC: 'NPMRC',
+  PROJECT_NAME: 'PROJECT_NAME',
+  MANAGER: 'MANAGER',
+  CLONE: 'CLONE',
+  INSTALLATION: 'INSTALLATION',
+}
+
+export const getStepsDetails = (name, boolean = true, title) => ({
+  [STEPS.AUTH]: {
     nr: 0,
     title: 'Authenticated',
-    success: true
+    success: boolean
   },
-  NPMRC: {
+  [STEPS.NPMRC]: {
     nr: 1,
-    title: 'Authenticated',
-    success: true
+    title: title || 'Install vsts-npm-auth',
+    success: boolean
   },
-  PROJECT_NAME: {
+  [STEPS.PROJECT_NAME]: {
     nr: 2,
     title: 'Project name',
-    success: true
+    success: boolean
   },
-  MANAGER:  {
+  [STEPS.MANAGER]:  {
     nr: 3,
     title: 'Selected package manager',
-    success: true
+    success: boolean
   },
-  CLONE:  {
+  [STEPS.CLONE]:  {
     nr: 4,
     title: 'Cloned repo',
-    success: true
+    success: boolean
   },
-  INSTALLATION:  {
+  [STEPS.INSTALLATION]:  {
     nr: 5,
     title: 'Installed dependencies',
-    success: true
+    success: boolean
   },
-}
+})[name]
--- hooks/useRunProject.js
+++ hooks/useRunProject.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { DataToolGenerator } from '../utils/dataToolTemplate.util';
 import { authenticate } from '../utils/auth.util';
-import { STEPS } from '../consts/steps.consts';
+import { STEPS, getStepsDetails } from '../consts/steps.consts';
 
 export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthenticated, setCompleted, projectName, manager, exit) => {
   const [os, setOs] = useState(undefined);
@@ -19,7 +19,7 @@ export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthen
   }
 
   useEffect(() => {
-    if (steps.length === STEPS.CLONE.nr) {
+    if (steps.length === getStepsDetails(STEPS.CLONE).nr) {
       getDataTool()
     }
   }, [steps])
@@ -28,7 +28,7 @@ export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthen
     setLoadingMsg('Authenticating')
     await authenticate()
     setUserIsAuthenticated(true);
-    updateSteps(STEPS.AUTH)
+    updateSteps(getStepsDetails(STEPS.AUTH))
     setLoadingMsg(undefined)
   }
 
@@ -36,13 +36,15 @@ export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthen
     const os = process.platform
     setOs(os)
     if (os === 'win32') {
-
+      updateSteps(getStepsDetails(STEPS.NPMRC))
+    } else {
+      updateSteps(getStepsDetails(STEPS.NPMRC, false, 'As you are not a Windows user, please check how to setup authentication to the feed here: https://docs.microsoft.com/en-us/azure/devops/artifacts/npm/npmrc?view=azure-devops#set-up-authentication-on-your-dev-box'))
     }
   }
 
   const _cloneProject = async (dataToolGenerator) => {
     await dataToolGenerator.cloneProject()
-    return updateSteps(STEPS.CLONE)
+    return updateSteps(getStepsDetails(STEPS.CLONE))
   }
 
   const _installProject = async (dataToolGenerator) => {
@@ -52,7 +54,7 @@ export const useRunProject = (steps, updateSteps, setLoadingMsg, setUserIsAuthen
   }
 
   const _finish = () => {
-    updateSteps(STEPS.INSTALLATION)
+    updateSteps(getStepsDetails(STEPS.INSTALLATION))
     setLoadingMsg(undefined)
     setCompleted(true)
     exit()
